0efd6b5622f9d53c48a6f7e28c160b36b743898c
SerhieievVolodymyr <vssergieiev@gmail.com>
2023-04-03 18:26:31 +0300

    Додав виконане завдання 1 Телефон

---
+++ hw15_phone.py
@@ -0,0 +1,13 @@
+class Phone:
+    phone_number = 0
+    __call_counter = 0
+
+    def add_number(self, num):
+        self.phone_number = num
+
+    def number_calls(self):
+        return self.__call_counter
+
+    def take_call(self):
+        self.__call_counter += 1
+
