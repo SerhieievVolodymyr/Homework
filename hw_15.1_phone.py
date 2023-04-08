ed8f433bb01e8a713810d92cbe9939c236953c09
SerhieievVolodymyr <vssergieiev@gmail.com>
2023-04-09 00:43:27 +0300

    додано метод __init__ створені об'єкти написана функція calls

--- hw_15.1_phone.py
+++ hw_15.1_phone.py
@@ -1,13 +1,41 @@
 class Phone:
-    phone_number = 0
-    _call_counter = 0
+    __call_counter = 0
+
+    def __init__(self, number):
+        self.phone_number = number
 
     def add_number(self, num):
         self.phone_number = num
 
     def number_calls(self):
-        return self._call_counter
+        return self.__call_counter
 
     def take_call(self):
-        self._call_counter += 1
+        self.__call_counter += 1
 
+
+samsung = Phone(380951111111)
+iphone = Phone(380952222222)
+pixel = Phone(380953333333)
+
+samsung.add_number(380951111112)
+iphone.add_number(380952222223)
+pixel.add_number(380953333334)
+
+samsung.take_call()
+
+iphone.take_call()
+iphone.take_call()
+
+pixel.take_call()
+pixel.take_call()
+pixel.take_call()
+
+phones = [samsung, iphone, pixel]
+
+
+def calls(phones_list):
+    calls_counter = 0
+    for phone in phones_list:
+        calls_counter += phone.number_calls()
+    return calls_counter
